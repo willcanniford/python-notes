81616d752e50f47b7644e023d29a68d9e0f66aa4
Will Canniford <willcanniford@yahoo.co.uk>
2018-12-01 11:05:45 +0000

    Add basic linear regression file to the machine learning section

---
+++ machine_learning/sklearn-lr.py
@@ -0,0 +1,40 @@
+# First example
+# Import the libraries that are going to be needed
+from sklearn.linear_model import LinearRegression
+
+# Initialise the model
+model = LinearRegression()
+
+# Define values; note that X must be a 2D array with each entry a representation of observations
+X = [[3], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1], [1]]
+
+# Outcome variable
+y = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+
+# Fit the model
+model.fit(X, y)
+
+# Make a prediction and print to the console
+print(model.predict([[2]]))
+
+
+# Second example from the documentation
+# Import numpy for this example
+import numpy as np
+
+# Generate observations and check the structure
+X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+print(X)
+
+# Generate outcome variable and check structure using defined mapping function
+y = np.dot(X, np.array([1, 2])) + 3
+print(y)
+
+# Initialise and fit the linear regression model
+reg = LinearRegression().fit(X, y)
+
+# View various model features 
+print("Model score: {}".format(reg.score(X, y)))
+print("Model coefficients: {}".format(reg.coef_))
+print("Model intercept: {}".format(reg.intercept_))
+print("Model predictions: {}".format(reg.predict(np.array([[3, 5], [2,3]]))))
