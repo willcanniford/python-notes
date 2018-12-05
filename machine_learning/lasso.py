cc461ab275289349c35b5605826a7f4a31bbeac5
Will Canniford <willcanniford@yahoo.co.uk>
2018-12-05 22:31:01 +0000

    Add notes about scaling data to the lasso script

--- machine_learning/lasso.py
+++ machine_learning/lasso.py
@@ -26,3 +26,27 @@ lasso_model = lasso_reg.fit(X, y)
 # We can access the coeficients through the `coef_` property
 # Note that this is a linear model, and isn't suited to this polynomial example data
 print(lasso_model.coef_)
+
+
+# Scaling data with sklearn
+from sklearn.preprocessing import StandardScaler
+
+# It is important when we are using regularisation or algorithms that use distance metrics to scale and standardise our features
+# This is to avoid the algorithm treating one feature more harshly than another based purely on the magnitude of the units it is recorded in
+
+# Create a StandardScaler object
+scaler = StandardScaler()
+
+scaled_X = scaler.fit_transform(X)
+
+# As our X variable wasn't that spread out, we don't see a massive difference in the resulting values
+print(scaled_X)
+print(X)
+
+# Let's try with the response variable purely for comparison
+scaled_y = scaler.fit_transform(pd.DataFrame(y))
+
+# We can see that the impact on y was much greater, and it now reflects the same scale seen in X
+# You wouldn't do this for your response variable, this was purely to demonstrate the impact of scaling the data
+print(scaled_y)
+print(y)
