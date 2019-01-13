5698e6a2f908a095e9bde9a1972bd95dd88bf999
Will Canniford <willcanniford@yahoo.co.uk>
2019-01-13 13:36:34 +0000

    Add a script defining the softmax function for multiclass classification problems

---
+++ deep_learning/softmax_function.py
@@ -0,0 +1,15 @@
+# the softmax function is equivalent to the sigmoid but is capable of taking more than two classes
+# it is an important step in having a continuous activation function inside a perceptron
+
+import numpy as np
+
+def softmax(L):
+    # we take the exponentials to make sure that all the linear scores are positive
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    results = []
+    # Loop through and take the proportions against the sum to give the probabilities of the given classes
+    for i in expL:
+        # Make sure that we are multiplying by a float to get the right output type
+        results.append(i*1.0/sumExpL)
+    return result
