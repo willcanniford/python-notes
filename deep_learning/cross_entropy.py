67513f10f589d50347ee77f6e12d5a1412428652
Will Canniford <willcanniford@yahoo.co.uk>
2019-01-13 14:00:53 +0000

    Add a script about two class cross entropy calculation

---
+++ deep_learning/cross_entropy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# This is for two classes for cross entropy
+# Write a function that takes as input two lists Y, P,
+# and returns the float corresponding to their cross-entropy.
+# Y represents the events, and P the probabilities
+def cross_entropy(Y, P):
+    # Make sure that the vectors are floats
+    Y = np.float_(Y)
+    P = np.float_(P)
+    # Note that if the event is 1 it takes the first part
+    # If the event is 0 it takes the value for q given that p and adds nothing for the first segment with p
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
