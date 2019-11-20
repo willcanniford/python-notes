8d1cdda34d49f25d0c86319fda18ee597eeae284
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-20 21:17:04 +0000

    Add file with remapping example using a dictionary

---
+++ programming/remapping_column.py
@@ -0,0 +1,14 @@
+# Remapping a column is a good way to change categorical data from one value to another 
+# Note that below we are changing a column containing bins to a numeric so we can summarise 
+# Print the unique values in 'stop_duration'
+print(ri.stop_duration.unique())
+
+# Create a dictionary that maps strings to integers
+mapping = {'0-15 Min':8, '16-30 Min':23, '30+ Min':45}
+
+# Convert the 'stop_duration' strings to integers using the 'mapping'
+ri['stop_minutes'] = ri.stop_duration.map(mapping)
+
+# Print the unique values in 'stop_minutes'
+print(ri['stop_minutes'].unique())
+print(ri['stop_minutes'].mean())
