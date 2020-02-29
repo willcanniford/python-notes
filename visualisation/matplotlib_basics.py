77f92646d15ff50c5e97d86a8c3e1c5849e4cc3c
Will Canniford <willcanniford@yahoo.co.uk>
2020-02-29 09:32:50 +0000

    Add information about setting labels
    - set_xlabel, set_ylabel and set_title examples while using the axes object generated by plt.subplots()

--- visualisation/matplotlib_basics.py
+++ visualisation/matplotlib_basics.py
@@ -16,4 +16,13 @@ numbers = [x for x in range(1, 10)]
 fig, ax = plt.subplots()
 
 ax.plot(numbers, color='red', marker='v', linestyle='--')
+
+'''
+You can add labels and titles to the plot using the set functions on ax
+'''
+
+ax.set_xlabel('Numbers (X)')
+ax.set_ylabel('Numbers (Y)')
+ax.set_title('Example plot with line styling')
+
 plt.show()
