ab801c8c0b655432e4fcb6b3106b98585b9a7feb
Will Canniford <willcanniford@yahoo.co.uk>
2020-09-27 13:50:52 +0100

    Create basic plot for simluating a 15 bet 'double your money'

--- sports_betting_sim/main.py
+++ sports_betting_sim/main.py
@@ -1,10 +1,22 @@
-from code.helpers import simulate_n_bets
+import matplotlib.pyplot as plt
+import numpy as np
+
+from code.helpers import simulate_n_bets, bank_over_time
 
 # Simulate 100 bets of 4/9 staking £5 each time
-n_100 = simulate_n_bets(100, 4 / 9, 5)
+n_100 = np.array(simulate_n_bets(100, 4 / 9, 5))
 
-# Calculate total winnings
-print(sum(n_100))
+# Calculate loss/gain
+n_100_lg = n_100 - 5
+n_100_lg = np.insert(n_100_lg, 0, 50)
 
-# Calculate number of bets won / 100
-print(len([i for i in n_100 if i > 0]))
+fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+x = np.arange(16)
+
+for _ in np.arange(1500):
+    plt.plot(x, bank_over_time(50, 15, 1, 5), c='grey', alpha=0.25, zorder=2)
+
+ax.axhline(50, c='red', zorder=1, lw=1, alpha=0.7)
+ax.set_xlim(0, None)
+ax.set_ylim(0, None)
+plt.show()
